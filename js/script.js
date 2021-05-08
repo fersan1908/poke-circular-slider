c2488597b41ba3a6b91bb69841ccb22f45399279
fersan1908 <67877952+fersan1908@users.noreply.github.com>
2021-05-08 02:21:10 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,52 @@
+const frontPic = document.getElementById("frontPic");
+const backPic = document.getElementById("backPic");
+const previous = document.getElementById("previous");
+const next = document.getElementById("next");
+const domCount = document.getElementById("count");
+const maxPictures = 150;
+let count = 1;
+
+//Chequear si hay imagenes siguientes y/o previas y desactivar botón si no hay mas;
+count == 1 ? previous.setAttribute("disabled", "disabled") : previous.removeAttribute("disabled", "disabled");
+count == maxPictures ? next.setAttribute("disabled", "disabled") : next.removeAttribute("disabled", "disabled");
+domCount.textContent = count;
+
+next.addEventListener("click", ()=> {
+    count++;
+    domCount.textContent = count;
+    //Mientras se ejecuta la animación desactivamos botones
+    previous.setAttribute("disabled", "disabled");
+    next.setAttribute("disabled", "disabled");
+    //animación
+    backPic.classList.add("change");
+    setTimeout(() => {
+        backPic.classList.remove("change");
+        frontPic.style.setProperty('--front-pic', `url(../assets/img/${count}.png)`);
+        count < maxPictures ? backPic.style.setProperty('--back-pic', `url(../assets/img/${count + 1}.png)`) : "";
+        //Chequear si hay imagenes siguientes y/o previas y desactivar botón si no hay mas;
+        count == 1 ? previous.setAttribute("disabled", "disabled") : previous.removeAttribute("disabled", "disabled");
+        count == maxPictures ? next.setAttribute("disabled", "disabled") : next.removeAttribute("disabled", "disabled");
+    }, 800);
+});
+
+previous.addEventListener("click", ()=> {
+    count--;
+    domCount.textContent = count;
+    //Mientras se ejecuta la animación desactivamos botones
+    previous.setAttribute("disabled", "disabled");
+    next.setAttribute("disabled", "disabled");
+    //Cambiar la posición de la nueva imagen y la anterior
+    frontPic.style.setProperty('--front-pic', `url(../assets/img/${count + 1}.png)`);
+    backPic.style.setProperty('--back-pic', `url(../assets/img/${count}.png)`);
+    //animacion
+    frontPic.classList.add("change__back");
+
+    setTimeout(() => {
+        frontPic.classList.remove("change__back");
+        frontPic.style.setProperty('--front-pic', `url(../assets/img/${count}.png)`);
+        backPic.style.setProperty('--back-pic', `url(../assets/img/${count + 1}.png)`);
+        //Chequear si hay imagenes siguientes y/o previas y desactivar botón si no hay mas;
+        count == 1 ? previous.setAttribute("disabled", "disabled") : previous.removeAttribute("disabled", "disabled");
+        count == maxPictures ? next.setAttribute("disabled", "disabled") : next.removeAttribute("disabled", "disabled");
+    }, 800);
+});
